3a1b42972f3e2f46a5adcda7b12548fb7089fe5d
Mathijs van de Ven <m.vandeven@rscmme.com>
2023-02-07 22:31:36 +1300

    feature: update special.py, rename dmfile_def to StudioFileStruct and change file definition functions to classmethods.

--- dmstudio/special.py
+++ dmstudio/special.py
@@ -1,58 +1,46 @@
-'''
-dmstudio.special
-================
+"""
 
 Python package with modified studio processes. The package is designed to make processes such as inpfil easier to use
 and to facilitate more readable code.
 
+"""
 
-
-'''
-
-import dmstudio.dmfiles
-import dmstudio.dmcommands
+# import dmstudio.dmfiles
+# import dmstudio.dmcommands
 import pandas as pd
 
-# -----------------------------------------------------------------------------------#
-# Special fields
-#------------------------------------------------------------------------------------#
-# certain fields are required to be 8 characters
-CHAR8_FIELDS = ['VALUE_IN', 'VALUE_OU', 'NUMSAM_F', 'SVOL_F', 'VAR_F', 'MINDIS_F']
+# dmf = dmstudio.dmfiles.init()
+# dmc = dmstudio.dmcommands.init()
 
-#  fields that are always implicit
-IMPLICIT_FIELDS = ['XMORIG', 'YMORIG', 'ZMORIG', 'NX', 'NY', 'NZ','X0','Y0','Z0','ANGLE1','ANGLE2','ANGLE3','ROTAXIS1','ROTAXIS2','ROTAXIS3']
-
-#------------------------------------------------------------------------------------#
-
-dmf = dmstudio.dmfiles.init()
-dmc = dmstudio.dmcommands.init()
-
-class dmfile_def(object):
+class StudioFileStruct(object):
 
     '''
-    dmfile_def
+    StudioFileStruct
     ----------
 
-    Class for interactively creating a datamine file definition as a pandas dataframe. The file definition is to be
-    used for an input for processes such as special.inpfil.
+    Class for creating a Studio file structure as a pandas dataframe. The file definition is to be
+    used for an input for file-handling commands such as INPFIL.
 
     Object Properties:
     ------------------
 
-    dmfile_def.definition: pandas dataframe
-        Dataframe to hold datamine file definition
+    StudioFileStruct.definition: pandas dataframe
+        Dataframe that holds the file specifications
 
 
     '''
+    # columns required for creating and importing files to Studio
+    columns = ['Field Name', 'Field Type', 'Length', 'Keep', 'Default']
 
     def __init__(self, definition=None):
 
-        columns = ['Field Name', 'Field Type', 'Length', 'Keep', 'Default']
-
         if definition is None:
-            self.definition = pd.DataFrame(columns=columns)
+            # Create empty file_definition
+            self.definition = pd.DataFrame(columns=self.columns)
+            
         else:
-            for col in columns:
+            #
+            for col in self.columns:
                 if col not in definition.columns:
                     raise "Column "  + col + " not found in definition. Columns 'Field Name', 'Field Type', 'Length'," \
                                              " 'Keep', 'Default' are required"
@@ -60,24 +48,96 @@ class dmfile_def(object):
 
 
     def add_field(self, field_name, field_type, length='', keep='Y', default=''):
+        """
+        Add a row to the StudioFileStruct by specifying field_name, type and length.
+        """
 
-        data = {'Field Name': field_name, 'Field Type': field_type, 'Length': length, 'Keep': keep,
-                'Default': default}
+        data = {'Field Name': field_name, 
+                'Field Type': field_type, 
+                'Length': length, 
+                'Keep': keep,
+                'Default': default
+                }
 
         dmtemp = pd.DataFrame(data)
-        field_order = ['Field Name', 'Field Type', 'Length', 'Keep', 'Default']
-        dmtemp = dmtemp[field_order]
+        dmtemp = dmtemp[self.columns]
         self.definition = self.definition.append(dmtemp).reset_index(drop=True)
 
-def inpfil(csv=None, out_o=None, definition=None):
+    @classmethod
+    def from_text(cls, filepath, **kwargs):
+        """
+        Create StudioFileStruct from comma-separated text file. 
+        A pandas dataframe is created first using read_csv, kwargs are passed 
+        directly to the read_csv method.
+        
+        Takes: filepath, the path to the comma-separated file
+        
+        Returns: a StudioFileStruct
+        
+        """
+        df = pd.read_csv(filepath, **kwargs)
 
-    if definition is None:
-        definition = csv_to_definition(csv)
+        return cls.from_dataframe(df)
+
+    @classmethod
+    def from_dataframe(cls, df, default = ''):
+        """
+        Create StudioFileStruct from pandas dataframe. Inspects each column and
+        records field_name, type and length. 
+        
+        Takes: a pandas DataFrame
+        
+        Returns: a StudioFileStruct
+        
+        """
+        definition = None
+        
+        field_names, field_type, length = [], [], []
+           
+        for column in df.columns:
+
+            field_names.append(column)
+
+            if df[column].dtype=='float64' or df[column].dtype=='int64':
+                field_type.append('N')
+                length.append('')
+            else:
+                field_type.append('A')
+                if column in CHAR8_FIELDS:
+                    length.append(8)
+                else:
+                    length.append(int((df[column].str.len().max()-1)/4+1)*4)
+
+        definition = pd.DataFrame({'Field Name': field_names, 'Field Type': field_type, 'Length': length})
+        definition['Keep'] = 'Y'
+        definition['Default'] = default
+
+        return cls(definition)
+
+def inpfil(csv, out_o, definition=None):
+    """
+    Imports text file to Studio using the INPFIL command. If a Studio file definition
+    is not provided as a StudioFileStruct, it is generated on the fly.
+    For special fields, the file definition is amended as required.
+    """
+
+    # These fields must be 8 characters
+    CHAR8_FIELDS = ['VALUE_IN', 'VALUE_OU', 'NUMSAM_F', 
+                    'SVOL_F', 'VAR_F', 'MINDIS_F']
+
+    # These fields are always implicit
+    IMPLICIT_FIELDS = ['XMORIG', 'YMORIG', 'ZMORIG', 'NX', 'NY', 'NZ',
+                       'X0','Y0','Z0','ANGLE1','ANGLE2','ANGLE3',
+                       'ROTAXIS1','ROTAXIS2','ROTAXIS3']   
 
     arguments = " 'csvfile' "
     df = pd.read_csv(csv)
 
-    for i in range(len(definition)):
+    if not isinstance(definition, StudioFileStruct)
+        # Invalid filedefinition, creating on the fly.
+        definition = StudioFileStruct.from_dataframe(df)
+
+    for i in range(definition.shape[0]):
 
         if definition['Field Name'].iloc[i] in CHAR8_FIELDS:
             definition['Field Type'].iloc[i] = 'A'
@@ -94,38 +154,6 @@ def inpfil(csv=None, out_o=None, definition=None):
     arguments += "'!' 'Y' " + csv
 
     dmf.inpfil(out_o=out_o, arguments=arguments)
-
-def csv_to_definition(csv):
-
-    df = pd.read_csv(csv)
-
-    return pd_to_definition(df);
-
-def pd_to_definition(df):
-
-    field_names = []
-    an = []
-    length = []
-
-    for column in df.columns:
-
-        field_names.append(column)
-
-        if df[column].dtype=='float64' or df[column].dtype=='int64':
-            an.append('N')
-            length.append('')
-        else:
-            an.append('A')
-            if column in CHAR8_FIELDS:
-                length.append(8)
-            else:
-                length.append(int((df[column].str.len().max()-1)/4+1)*4)
-
-    definition = pd.DataFrame({'Field Name': field_names, 'Field Type': an, 'Length': length})
-    definition['Keep']='Y'
-    definition['Default']=''
-
-    return definition;
     
     
 
